ee998a982f5260483b74d8dfb83c4ed1ee02dbf5
ZeOne <zev.rochon@gmail.com>
2026-08-05 22:06:02 -0500

    Fix weak reference syntax in _startCamera method

--- Sources/VZEnrollmentViewController.mm
+++ Sources/VZEnrollmentViewController.mm
@@ -222,7 +222,7 @@ static const NSInteger kSamplesPerStage = 2;
 
 - (void)_startCamera {
     _camera = [VZCameraController sharedController];
-    __weak typeof(self) weak = self;
+    __weak __typeof__(self) weak = self;
     _camera.stateBlock = ^(VZCameraState state, NSError *err) {
         if (state == VZCameraStateRunning) {
             dispatch_async(dispatch_get_main_queue(), ^{
